ce0da3f79535244ec72e0322ef47132dcd6886e6
michael <michael.schlafke@gmail.com>
2023-04-24 14:02:51 +0200

    fixed imports

--- gym_brt/reinforcement_learning/dql.py
+++ gym_brt/reinforcement_learning/dql.py
@@ -4,9 +4,11 @@ https://pytorch.org/tutorials/intermediate/reinforcement_q_learning.html
 changed: - the environment to the QubeSwingupEnv
 """
 
-import gym_brt.envs.qube_swingup_env as qse
+#import gym_brt.envs.qube_swingup_env as qse
+import gym
+from gym_brt.envs.qube_swingup_env import QubeSwingupEnv
 
-import gymnasium as gym
+#import gymnasium as gym
 import math
 import random
 import matplotlib
@@ -19,7 +21,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
 
-env = qse.QubeSwingupEnv()
+env = QubeSwingupEnv(use_simulator=True)
 
 # set up matplotlib
 is_ipython = 'inline' in matplotlib.get_backend()
@@ -83,9 +85,9 @@ TAU = 0.005
 LR = 1e-4
 
 # Get number of actions from gym action space
-n_actions = env.action_space.n
+n_actions = env.action_space.n  # TODO: make compatible with continuous action space
 # Get the number of state observations
-state, info = env.reset()
+state = env.reset()
 n_observations = len(state)
 
 policy_net = DQN(n_observations, n_actions).to(device)
@@ -198,48 +200,48 @@ else:
 # render each episode?
 renderer = True
 
-with QubeSwingupEnv(use_simulator=True) as env:
-    for i_episode in range(num_episodes):
-        # Initialize the environment and get it's state
-        state, info = env.reset()
-        state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-        for t in count():
-            action = select_action(state)
-            observation, reward, terminated, truncated, _ = env.step(action.item())
-            reward = torch.tensor([reward], device=device)
-            done = terminated or truncated
+#with QubeSwingupEnv(use_simulator=True) as env:
+for i_episode in range(num_episodes):
+    # Initialize the environment and get it's state
+    state = env.reset()
+    state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    for t in count():
+        action = select_action(state)
+        observation, reward, terminated, truncated, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+        done = terminated or truncated
 
-            if terminated:
-                next_state = None
+        if terminated:
+            next_state = None
 
-            # renderer used in test.py
-            if render:
-                env.render()
+        # renderer used in test.py
+        if renderer:
+            env.render()
 
-            else:
-                next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
+        else:
+            next_state = torch.tensor(observation, dtype=torch.float32, device=device).unsqueeze(0)
 
-            # Store the transition in memory
-            memory.push(state, action, next_state, reward)
+        # Store the transition in memory
+        memory.push(state, action, next_state, reward)
 
-            # Move to the next state
-            state = next_state
+        # Move to the next state
+        state = next_state
 
-            # Perform one step of the optimization (on the policy network)
-            optimize_model()
+        # Perform one step of the optimization (on the policy network)
+        optimize_model()
 
-            # Soft update of the target network's weights
-            # θ′ ← τ θ + (1 −τ )θ′
-            target_net_state_dict = target_net.state_dict()
-            policy_net_state_dict = policy_net.state_dict()
-            for key in policy_net_state_dict:
-                target_net_state_dict[key] = policy_net_state_dict[key] * TAU + target_net_state_dict[key] * (1 - TAU)
-            target_net.load_state_dict(target_net_state_dict)
+        # Soft update of the target network's weights
+        # θ′ ← τ θ + (1 −τ )θ′
+        target_net_state_dict = target_net.state_dict()
+        policy_net_state_dict = policy_net.state_dict()
+        for key in policy_net_state_dict:
+            target_net_state_dict[key] = policy_net_state_dict[key] * TAU + target_net_state_dict[key] * (1 - TAU)
+        target_net.load_state_dict(target_net_state_dict)
 
-            if done:
-                episode_durations.append(t + 1)
-                plot_durations()
-                break
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            break
 
     print('Complete')
     plot_durations(show_result=True)
